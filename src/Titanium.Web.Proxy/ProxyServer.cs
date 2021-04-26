1fcd5d5380ea0503e910ccd35ecdd5fe1fc9857c
Sebastian Solnica <ssolnica@gmail.com>
2021-04-26 17:42:10 +0200

    Fix for #840: handling exceptions thrown when server is stopping

--- src/Titanium.Web.Proxy/ProxyServer.cs
+++ src/Titanium.Web.Proxy/ProxyServer.cs
@@ -244,6 +244,7 @@ namespace Titanium.Web.Proxy
         /// </summary>
 #pragma warning disable 618
         public SslProtocols SupportedSslProtocols { get; set; } = SslProtocols.Ssl3 | SslProtocols.Tls | SslProtocols.Tls11 | SslProtocols.Tls12;
+#pragma warning restore 618
 
         /// <summary>
         ///     The buffer pool used throughout this proxy instance.
